f6b61248e346e54ee43597b343d5d4954796a0e4
Austin Drury <drurya96@gmail.com>
2026-06-01 20:20:07 -0500

    fix

--- generate-gallery.js
+++ generate-gallery.js
@@ -37,8 +37,9 @@ function readPrice(folderPath) {
   }
 
   return "$" + price;
- }
-  function readDate(folderPath) {
+}
+
+function readDate(folderPath) {
   const dateFile = path.join(folderPath, "date.txt");
 
   if (!fs.existsSync(dateFile)) {
@@ -54,7 +55,6 @@ function readPrice(folderPath) {
   return date;
 }
 
-
 const items = fs
   .readdirSync(imagesRoot, { withFileTypes: true })
   .filter(entry => entry.isDirectory())
@@ -62,40 +62,21 @@ const items = fs
     const folder = entry.name;
     const folderPath = path.join(imagesRoot, folder);
 
-    const imageFiles = fs
-  .readdirSync(folderPath, { withFileTypes: true })
-  .filter(file => file.isFile())
-  .map(file => file.name)
-  .filter(fileName => allowedExtensions.has(path.extname(fileName).toLowerCase()));
+    const images = fs
+      .readdirSync(folderPath, { withFileTypes: true })
+      .filter(file => file.isFile())
+      .map(file => file.name)
+      .filter(fileName => allowedExtensions.has(path.extname(fileName).toLowerCase()))
+      .sort((a, b) => a.localeCompare(b, undefined, { numeric: true }));
 
-const images = imageFiles
-  .sort((a, b) => a.localeCompare(b, undefined, { numeric: true }));
-
-const gitLogFile = path.join(folderPath, imageFiles[0]);
-
-let newestImageTime = 0;
-
-try {
-  const { execSync } = require("child_process");
-
-  newestImageTime = Number(
-    execSync(
-      `git log -1 --format=%ct -- "${gitLogFile}"`,
-      { encoding: "utf8" }
-    ).trim()
-  );
-} catch {
-  newestImageTime = 0;
-}
-
-   return {
-  id: idFromFolder(folder),
-  name: displayNameFromFolder(folder),
-  folder,
-  price: readPrice(folderPath),
-  sortDate: readDate(folderPath),
-  images
-};
+    return {
+      id: idFromFolder(folder),
+      name: displayNameFromFolder(folder),
+      folder,
+      price: readPrice(folderPath),
+      sortDate: readDate(folderPath),
+      images
+    };
   })
   .filter(item => item.images.length > 0);
 
